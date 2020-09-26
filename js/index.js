551a331fc2093a52d3f0109a3653e61646563bf3
Aimee Lynn Ramirez <alramirez1@student.fullsail.edu>
2020-09-26 08:15:08 -0400

    commit the forms and api TODO cite

--- js/index.js
+++ js/index.js
@@ -1,5 +1,158 @@
-'user strict';
+"user strict";
+const include = (file) => {
+  let script = document.createElement("script");
+  script.src = file;
+  //script.crossorigin = "anonymous";
+  script.defer = true;
 
-window.onload = () =>{
+  getScript(script);
+};
+const getScript = (script) => {
+  let htmlScript = document.querySelector("body");
+  htmlScript.insertAdjacentElement("beforeend", script);
+};
+/**
+ * Create form fields
+ */
+let app = document.getElementById("app");
+app.innerHTML += `<div> <strong>Open API: Project + Portfolio Demo</strong></div>`;
 
-}
+let createForm = document.createElement("form");
+let createInput = document.createElement("input");
+createInput.id = "address";
+createForm.innerHTML += `<div> <h3>Weather API </h3><br/> Search a location</div>`;
+let createButtonSubmit = document.createElement("button");
+app.insertAdjacentElement("beforeend", createForm);
+createForm.insertAdjacentElement("beforeend", createInput);
+createForm.insertAdjacentElement("beforeend", createButtonSubmit);
+createButtonSubmit.innerHTML += `Submit`;
+
+/**
+ * Create table and get information
+ * from https://weather-gov.github.io/api/gridpoints
+ * api.weather.gov
+ */
+let createDivLatLong = document.createElement("div");
+createDivLatLong.id = "latLong";
+app.insertAdjacentElement("beforeend", createDivLatLong);
+let createDivLoc = document.createElement("div");
+app.insertAdjacentElement("beforeend", createDivLoc);
+let createDiv = document.createElement("div");
+app.insertAdjacentElement("beforeend", createDiv);
+let createTable = document.createElement("table");
+app.insertAdjacentElement("beforeend", createTable);
+
+const submitForm = (e) => {
+  e.preventDefault();
+// to clear tables if to be submitting again
+  createDivLatLong.innerHTML = "";
+  createDivLoc.innerHTML = "";
+  createDiv.innerHTML = "";
+  createTable.innerHTML = "";
+
+  let getAddress = document.getElementById("address");
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    encodeURIComponent(getAddress.value) +
+    ".json?access_token=pk.eyJ1IjoiYWltZWVseW5ucmFtaXJlejMiLCJhIjoiY2s3MXpjdXhoMGF3YjNtbXl6em9nMWRtbCJ9.atrDeTFuHq0gGUwi5Kq1_w";
+
+  class Geocode {
+    constructor(url) {
+      this.url = url;
+    }
+    loadData() {
+      fetch(this.url)
+        .then((response) => response.json())
+        .then((data) => {
+          let featuresLat = data.features[0].center[1];
+          let featuresLong = data.features[0].center[0];
+
+          let locations = data.features[0].place_name;
+          createDivLatLong.innerHTML = `<p><strong>GeoLocation: </strong><br/> latitude: ${featuresLat}, longitude:${featuresLong}</p>`;
+          createDivLoc.innerHTML = `<p> Location: ${locations} </p>`;
+
+          let url =
+            "https://api.weather.gov/points/" +
+            data.features[0].center[1] +
+            "," +
+            data.features[0].center[0];
+
+        //   let options = {
+        //     header: {
+        //       "Access-Control-Allow-Origin": "http://localhost:5500",
+        //     },
+        //   };
+          fetch(url)
+            .then((response) => response.json())
+            .then((weather) => {
+            //   console.log(weather);
+              createDiv.innerHTML =
+                "<h4> Relative Location: " +
+                weather.properties.relativeLocation.properties.city +
+                ", " +
+                weather.properties.relativeLocation.properties.state +
+                "</h4>";
+
+              let forecastUrl = weather.properties.forecast;
+
+              fetch(forecastUrl)
+                .then((response) => response.json())
+                .then((weatherDaily) => {
+                  createTable.innerHTML += `
+                        <thead>
+                          <tr>
+                            <th>Image</th>
+  
+                            <th >#</th>
+                            <th >Day</th>
+                            <th>Start Time</th>
+                            <th>End Time</th>
+                            <th >Temperature</th>
+
+                          </tr>
+                        </thead>
+                      `;
+                  for (let i = 0; i < weatherDaily.properties.periods.length; i++) {
+                    let createTr = document.createElement("tr");
+                    createTable.insertAdjacentElement("beforeend", createTr);
+
+                    let start = new Date(
+                      weatherDaily.properties.periods[i].startTime
+                    );
+                    let ended = new Date(
+                      weatherDaily.properties.periods[i].endTime
+                    );
+                    createTr.innerHTML +=
+                      "<img src='" +
+                      weatherDaily.properties.periods[i].icon +
+                      "'style='width:100px;'>" +
+                      "<td>" +
+                      weatherDaily.properties.periods[i].number +
+                      " </td> " +
+                      "<td>" +
+                      weatherDaily.properties.periods[i].name +
+                      " </td> " +
+                      "<td>" +
+                      start +
+                      " </td><td>" +
+                      ended +
+                      " </td><td>" +
+                      weatherDaily.properties.periods[i].temperature +
+                      " " +
+                      weatherDaily.properties.periods[i].temperatureUnit +
+                      "°" +
+                      "</td>";
+
+                  }
+                });
+            })
+            .catch(function () {
+              console.log("error");
+            });
+        });
+    }
+  }
+  let getFetch = new Geocode(url);
+  getFetch.loadData();
+};
+createButtonSubmit.addEventListener("click", submitForm);
